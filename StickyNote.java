4902dc40b25c9286b823602072dfe8f355372d3b
Wesley Chang <wesley.chang@berkeley.edu>
2025-10-25 14:19:34 -0700

    Fleshing out StickyNote object

--- StickyNote.java
+++ StickyNote.java
@@ -1,5 +1,31 @@
-public class StickyNote {
-  //variables
-  public StickyNote();
+package StickyNoteAR;
 
+public class StickyNote {
+  int[] color = new int[3];
+  Double[] location = new Double[3];
+  Double[] normal = new Double[3];
+  String data;
+
+  public StickyNote(int[] color, Double[] location, Double[] normal, String data) {
+      this.color = color;
+      this.location = location;
+      this.normal = normal;
+      this.data = data;
+  }
+
+  public int[] getColor() {
+      return color;
+  }
+
+  public Double[] getLocation() {
+      return location;
+  }
+
+  public Double[] getNormal() {
+      return normal;
+  }
+
+  public String getData() {
+      return data;
+  }
 }
